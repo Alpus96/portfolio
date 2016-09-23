b39659bbd7ccbe1c97dea4062bb9e3d3d8e79335
Alpus96 <alex.perme@gmail.com>
2016-09-23 13:44:19 +0200

    saving changes

--- login.php
+++ login.php
@@ -1,72 +1,42 @@
-<html>
-<header>
-	<!--site information-->
-	<meta charset="UTF-8">
-	<meta name="description" content="My portfolio...">
-	<meta name="keywords" content="portfolio, HTML, CSS, PHP, JS, JavaScript, Java, C#, C++, C">
-	<meta name="author" content="Alexander Ljungberg Perme">
-	<!--tabicon! + linked files-->
-	<link rel="icon" href="">
-	<link rel="stylesheet" type="text/css" href="">
-	<!--page title-->
-	<title>Portfolio</title>
-</header>
-
-<body>
-	<!--php-->
-	<?php
-		session_start();
+<?php
+	session_start();
 	
+	include_once('login.html');
+	
+	function login{
+		$resp = "Incorrect details!";
+		function resp();
 		if($_LOGIN['login']){
 			include_once('db.php');
 			$username = strip_tags($_LOGIN['username']);
 			$password = strip_tags($_LOGIN['password']);
-		
+	
 			$username = stripslashes($username);
 			$password = stripslashes($password);
-		
+	
 			$username = mysqli_real_escape_string($username);
 			$password = mysqli_real_escape_string($password);
-		
+	
 			$password = md5($password);
-		
+	
 			$sql = "SELECT * FROM " & $table & " WHERE " & $column & " = '$username' LIMIT 1";
 			$query = mysqli_query($db, $sql);
 			$row = mysqli_fetch_array($query);
 			$id = $row['id'];
 			$db_password= $row['passowrd'];
-		
+	
 			if($password == $db_password){
 				$_SESSION['username'] = $username;
 				$_SESSION['id'] = $id;
 				header("Location: index.php");
 			}else{
-				echo "You entered the wrong details!";
+				$resp = "Incorrect details!";
+				function resp();
 			}
 		}
-		//a change
-		if(!$db_password){
+	}
+	
+	function register(){
 		
-		}
-	?>
-	<!--Meny-->
-	<nav class="menu">
-	
-	</nav>
-
-	<!--Sidans kropp/innehåll-->
-	<div class="body">
-		<!--login form-->
-		<form action="login.php" method="login" enctype="multitype/form-data">
-			<input placeholder="Username" name="username" type="text" autofocus>
-			<input placeholder="Password" name="password" type="text">
-			<input name="login" type="submit" value="Login">
-		</form>
-	</div>
-
-	<!--sidans fot-->
-	<div class="footer">
-	
-	</div>
-</body>
-</html>
+	}
+?>
